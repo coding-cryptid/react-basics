16c618390657119cfdfb8fa5f07185c11cb86d6b
Gianna Cosenza <giacosey28@gmail.com>
2026-05-14 10:50:59 -0400

    added a list element in script.jsx

--- script.jsx
+++ script.jsx
@@ -2,3 +2,14 @@ import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<p>Hello world</p>);
 
+ReactDOM.createRoot(document.querySelector('#root')).render(
+  <div>
+    <h1>My List of Food</h1>
+    <ul>
+      <li>Cheese Burger</li>
+      <li>Spicy Boneless Wings</li>
+      <li>Chorizo Tacos</li>
+    </ul>
+  </div>
+);
+);
